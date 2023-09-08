dbb6b74da20776b7fc40d6a06bee583996d630fe
MohammedGhazaly <warendragon67@gmail.com>
2023-09-08 22:43:36 +0200

    Created custom rotated Sebha

--- lib/screens/tasbeeh/tasbeeh_screen.dart
+++ lib/screens/tasbeeh/tasbeeh_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/screens/tasbeeh/rotated_sebha_widget.dart';
 
-class TasbeehScreen extends StatelessWidget {
+class TasbeehScreen extends StatefulWidget {
   const TasbeehScreen({super.key});
 
   @override
+  State<TasbeehScreen> createState() => _TasbeehScreenState();
+}
+
+class _TasbeehScreenState extends State<TasbeehScreen> {
+  double turns = 0.0;
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        children: [
+          createAnimatedSebha(
+              turns: turns,
+              onTapFunction: () {
+                setState(() {
+                  turns += 1 / 33;
+                });
+              }),
+        ],
+      ),
+    );
   }
 }
